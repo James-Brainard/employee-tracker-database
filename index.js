c975d00307c99a68752c7cdd13b0dfaf477bbdac
James Brainard <james.brainard2@aol.com>
2023-09-24 21:08:49 -0700

    modified index to properly display roles, and manager names when adding an employee.

--- index.js
+++ index.js
@@ -71,127 +71,138 @@ promptUser = () => {
         })
 
       } else if (menuChoice === "Add Employee") {
+        // Make if (err) a two liner with an else. 
+        // After the else define 
         db.query("SELECT * FROM roles", (err, res) => {
-          if (err) throw err;
-          inquirer.prompt([
-            {
-              message: "What is the employees first name?",
-              name: "firstName",
-              validate: firstName => {
-                if (firstName) {
-                  return true;
-                } else {
-                  console.log("Please enter a first name.");
-                  return false;
-                }
+          if (err) {
+            throw err;
+          } else {
+            const roleArr = res.map(role => role.title)
+            db.query("SELECT * FROM employee", (err, eres) => {
+              if (err) {
+                throw err;
+              } else {
+                const manArr = eres.map(manager => manager.first_name);
+
+                inquirer.prompt([
+                  {
+                    message: "What is the employees first name?",
+                    name: "firstName",
+                    validate: firstName => {
+                      if (firstName) {
+                        return true;
+                      } else {
+                        console.log("Please enter a first name.");
+                        return false;
+                      }
+                    }
+                  },
+                  {
+                    message: "What is the employees last name?",
+                    name: "lastName",
+                    validate: lastName => {
+                      if (lastName) {
+                        return true;
+                      } else {
+                        console.log("Please enter a last name.");
+                        return false;
+                      }
+                    }
+                  },
+                  {
+                    type: "list",
+                    message: "What is the employees role?",
+                    name: "employeeRole",
+                    choices: roleArr
+                    // () => {
+                    // let roleArr = [];
+                    // for (let i = 0; i < res.length; i++) {
+                    //   roleArr.push(res[i].name);
+                    // }
+                    // return roleArr;
+                    // }
+                  },
+                  {
+                    type: "list",
+                    message: "What is the employees manager?",
+                    name: "employeeManager",
+                    choices: manArr
+                  }
+                ])
+                  .then((answers) => {
+                    for (let i = 0; i < res.length; i++) {
+                      if (res[i].title === answers.employeeRole) {
+                        console.log(res[i]);
+                        var employeeRole = res[i];
+                      }
+                    }
+                    let { firstName, lastName, employeeManager } = answers;
+                    db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`, [firstName, lastName, employeeRole.id, employeeManager.id], (err, res) => {
+                      if (err) throw err;
+                      console.log(`Added ${firstName} ${lastName} to the database.`)
+                      promptUser();
+                    })
+                  })
               }
-            },
-            {
-              message: "What is the employees last name?",
-              name: "lastName",
-              validate: lastName => {
-                if (lastName) {
-                  return true;
-                } else {
-                  console.log("Please enter a last name.");
-                  return false;
-                }
-              }
-            }, 
-            {
-              message: "What is the employees role?",
-              name: "employeeRole",
-              Choices: () => {
-                let roleArr = [];
-                for (let i = 0; i < res.length; i++) {
-                  roleArr.push(res[i].name);
-                }
-                return roleArr;
-              }
-            },
-            {
-              message: "What is the employees manager?",
-              name: "employeeManager",
-              validate: employeeManagerInput => {
-                if (employeeManagerInput) {
-                  return true;
-                } else {
-                  console.log("Please input the employees manager.");
-                  return false;
-                }
-              }
-            }
-          ])
-          .then((answers) => {
-            for (let i = 0; i < res.length; i++) {
-              if (res[i].title === answers.employeeRole) {
-                console.log(res[i]);
-                var employeeRole = res[i];
-              }
-            }
-            db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`, [answers.firstName, answers.lastName, employeeRole.id, answers.employeeManager.id], (err, res) => {
-              if (err) throw err;
-              console.log(`Added ${answers.firstName} ${answers.lastName} to the database.`)
-              promptUser();
             })
-          })
+          }
         });
 
       } else if (menuChoice === "Add a Role") {
         db.query(`SELECT * FROM department`, (err, res) => {
           if (err) throw err;
-        inquirer.prompt([
-          {
-            message: "What role would you like to add?",
-            name: "newRole",
-            validate: newRoleInput => {
-              if (newRoleInput) {
-                return true;
-              } else {
-                console.log("Please enter a name for the role.");
-                return false;
+          inquirer.prompt([
+            {
+              message: "What role would you like to add?",
+              name: "newRole",
+              validate: newRoleInput => {
+                if (newRoleInput) {
+                  return true;
+                } else {
+                  console.log("Please enter a name for the role.");
+                  return false;
+                }
               }
-            }
-          }, 
-          {
-            message: "What is the salary for this role?",
-            name: "salary",
-            validate: salaryInput => {
-              if (salaryInput) {
-                return true;
-              } else {
-                console.log("Please input a salary for this role.");
-                return false;
+            },
+            {
+              message: "What is the salary for this role?",
+              name: "salary",
+              validate: salaryInput => {
+                if (salaryInput) {
+                  return true;
+                } else {
+                  console.log("Please input a salary for this role.");
+                  return false;
+                }
               }
-            }
-          },
-          {
-            type: "list",
-            message: "What department does this role belong to?",
-            name: "department",
-            choices: () => {
-              let deptArr = [];
+            },
+            {
+              type: "list",
+              message: "What department does this role belong to?",
+              name: "department",
+              choices: () => {
+                let deptArr = [];
+                for (let i = 0; i < res.length; i++) {
+                  deptArr.push(res[i].name);
+                }
+                return deptArr;
+              }
+            },
+          ])
+            .then((answers) => {
               for (let i = 0; i < res.length; i++) {
-                deptArr.push(res[i].name);
+                if (res[i].name === answers.department) {
+                  console.log(res[i]);
+                  var department = res[i];
+                }
               }
-              return deptArr;
-            }
-          },
-        ]) 
-        .then((answers) => {
-          for (let i = 0; i < res.length; i++) {
-            if (res[i].name === answers.department) {
-              console.log(res[i]);
-              var department = res[i];
-            }
-          }
-          db.query(`INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)`, [answers.newRole, answers.salary, department.id], (err, res) => {
-            if (err) throw err;
-            console.log(`Added ${answers.newRole} to the database.`);
-            promptUser();
-          })
-        })
-      });
+              db.query(`INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)`, [answers.newRole, answers.salary, department.id], (err, res) => {
+                if (err) throw err;
+                console.log(`Added ${answers.newRole} to the database.`);
+                promptUser();
+              })
+            })
+        });
       } else if (menuChoice === "Add Department") {
         inquirer.prompt([
           {
@@ -206,7 +217,7 @@ promptUser = () => {
               }
             }
           }
-        ]) .then((answers) => {
+        ]).then((answers) => {
           db.query(`INSERT INTO department (name) VALUES (?)`, [answers.newDepartment], (err, res) => {
             if (err) throw err;
             console.log(`Added ${answers.newDepartment} to the database.`);
